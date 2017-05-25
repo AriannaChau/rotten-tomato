ba3cb2517c5cf98ebad87805987e4b82726cedde
Arianna Chau and Karina Roush <arianna@>
2017-05-25 14:21:12 -0700

    get table to sort by movie score

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,49 @@
+// BACK END LOGIC //
+var movieArray = [];
+// constructor for userMovies
+function userMovie(movieName, movieYear, movieScore) {
+  this.movieName = movieName;
+  this.movieYear = movieYear;
+  this.movieScore = movieScore;
+}
 
-// USER INTERFACE LOGIC
+
+// USER INTERFACE LOGIC //
+
 $(document).ready(function() {
+// empty array for scores to be pushed into
+  var scoreArray = [];
+
   $("#movieForm").submit(function(event) {
     event.preventDefault();
 
-    var movieName = $("#userInputName").val();
-    var movieYear = $("#userInputYear").val();
-    var movieScore = $("#userInputScore").val();
+    // gathering user inputs from form
+    var inputName = $("input#userInputName").val();
+    var inputYear = $("input#userInputYear").val();
+    var inputScore = parseInt($("input#userInputScore").val());
 
-    var userMovie = movieName + movieYear + movieScore;
-    $("#results").append('<tr>' + '<td>' + movieName + '</td>' + '<td>' + movieYear + '</td>' + '<td>' + movieScore + '</td>' + '</tr>');
+    var newMovie = new userMovie(inputName, inputYear, inputScore);
+    movieArray.push(newMovie);
+
+    // pushing movie score values into empty scoreArray
+    scoreArray.push(inputScore);
+
+    // function to sort numerically
+    var sortedList = movieArray.slice(0);
+    sortedList.sort(function(a, b) {
+      return b.movieScore - a.movieScore;
+    });
+
+    $("#results").empty();
+
+    // appends results to table
+    for (i =0; i < sortedList.length; i++) {
+    $("#results").append('<tr>' +
+    '<td>' + sortedList[i].movieName + '</td>' +
+    '<td>' + sortedList[i].movieYear + '</td>' +
+    '<td>' + sortedList[i].movieScore + '</td>' +
+    '</tr>');
+  };
 
   });
 });
